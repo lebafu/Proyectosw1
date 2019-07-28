045cea6dec05a54375b2cf2639d37c2e9407c9fd
lebafu <50503857+lebafu@users.noreply.github.com>
2019-07-28 14:34:46 -0400

    actalista

--- tesis/resources/views/tesis/acta_examen.blade.php
+++ tesis/resources/views/tesis/acta_examen.blade.php
@@ -23,36 +23,53 @@
 			<h4 style="margin-bottom: 20px; margin-top: 10px">{{$tes->profesor3_comision}}</h4>
 		@endif
 
+		@if($tes->profesor1_externo==null)
+
+		@else	
+			<h4 style="margin-bottom: 20px; margin-top: 10px">{{$tes->profesor1_externo}}</h4>
+		@endif
+
+		@if($tes->profe2_externo==null)
+
+		@else	
+			<h4 style="margin-bottom: 20px; margin-top: 10px">{{$tes->profe2_externo}}</h4>
+		@endif
+
+
 			
 			
 
 			<h4>Profesor Guia:</h4>
-			<h4>{{$tes->profesor_guia}}</h4>
-		<h4>Para evaluar la Exposición y Defensa del examen de titulo denominado:</h4>
+			<h4 style="margin-bottom: 20px; margin-top: 10px">{{$tes->profesor_guia}}</h4>
+
+		<h4 style="margin-bottom: 40px; margin-top: 30px" >Para evaluar la Exposición y Defensa del examen de titulo denominado:</h4>
 		
-		<h4>Tema:"{{$tes->nombre_tesis}}"</h4>
+		<h4 style="margin-bottom: 40px; margin-top: 30px">Tema:"{{$tes->nombre_tesis}}"</h4>
+
+		<h4 style="margin-bottom: 20px; margin-top: 10px">{{$tes->nombre_completo}}   {{$tes->rut}}<h4>
+
+		<h4>Realizada la exposición se concluye que la nota final de la exposicion es:</h4>
 
 
-		<table class="table table-bordered">
-        <tr>
-         
-          <th>ID</th>
-          <th>Nombre Estudiante</th>
-          <th>Profesor Guia</th>
-          <th>Tipo Trabajo</th>
-          <th>Carrera<th>
-        </tr>
-      
-        <tr>
-          <td>{{$tes->id}}</td>
-          <td>{{$tes->nombre_completo}}</td>
-          <td>{{$tes->profesor_guia}}</td>
-         <td>{{$tes->tipo}}</td>
-         <td>{{$tes->carrera}}</td>
-          </tr>
+		<h4>........................................</h4>
+		<h4 style="margin-bottom: 40px; margin-top: 30px">{{$tes->profesor_guia}}</h4>
 
-        
-     </table>
+		<h4>........................................</h4>
+		<h4 style="margin-bottom: 40px; margin-top: 30px">{{$tes->profesor1_comision}}</h4>
+		<h4>......................................</h4>
+		<h4 style="margin-bottom: 40px; margin-top: 30px">{{$tes->profesor2_comision}}</h4>
+		@if($tes->profesor3_comision!="Ninguno")
+		<h4>........................................</h4>
+		<h4 style="margin-bottom: 40px; margin-top: 30px">{{$tes->profesor3_comision}}</h4>
+		@endif
+		@if($tes->profesor1_externo!=null)
+		<h4>......................................</h4>
+		<h4 style="margin-bottom: 40px; margin-top: 30px">{{$tes->profesor1_externo}}</h4>
+		@endif
+		@if($tes->profe2_externo!=null)
+		<h4>......................................</h4>
+		<h4 style="margin-bottom: 40px; margin-top: 30px">{{$tes->profesor1_externo}}</h4>
+		@endif
 	@endforeach
 	<script>
 		
